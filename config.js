96e64dc39349786b3ad3755e3a6148979cdbbccd
Ajibare Kehinde <155313217+Korretdesigns@users.noreply.github.com>
2024-05-12 16:56:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.owner = [['917605902011', 'GURU', true], ['6281255369012', 'Ash', true], 
 
 //global.pairingNumber = "2349067339193" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
+global.mods = ['2349067339193', '2348034779798']
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
